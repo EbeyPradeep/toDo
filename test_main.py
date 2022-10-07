5a8caa06a86195ec19cecd078dd51459786daddf
Ebey <007ebey@gmail.com>
2022-10-07 11:49:51 +0530

    should work

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import add_numbers
 
 
 class Test:
-    test_cases = [(1, 3, 4), (3, 4, 7), (3, 5, 7)]
+    test_cases = [(1, 3, 4), (3, 4, 7), (3, 5, 8), (4, 2, 6)]
 
     @pytest.mark.parametrize("a,b, expected_result", test_cases)
     def test_add_numbers(self, a, b, expected_result):
